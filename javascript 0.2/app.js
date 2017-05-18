02fabf7e7afd623ade54ec1e49159a30b1746663
freakyfrog69 <denisas.jersovas@gmail.com>
2017-05-18 21:25:11 +0300

    add query selector for html element #current-0 and manipulated DOM

--- javascript 0.2/app.js
+++ javascript 0.2/app.js
@@ -8,3 +8,14 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+var score, roundeScore, activePlayer, dice;
+
+scores = [0, 0];
+roundeScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6 ) + 1;
+
+//document.querySelector('#current-'+ activePlayer).textContent = dice;
+
+document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
