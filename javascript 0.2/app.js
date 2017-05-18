68e9d5e49c20c626b14a8a2f0f8399ef2041eb5c
freakyfrog69 <denisas.jersovas@gmail.com>
2017-05-18 22:17:23 +0300

    add event listener to the button , with a annonymouse function

--- javascript 0.2/app.js
+++ javascript 0.2/app.js
@@ -8,14 +8,63 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score, roundeScore, activePlayer, dice;
+var score, roundeScore, activePlayer;
 
 scores = [0, 0];
-roundeScore = 0;
+roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6 ) + 1;
 
-//document.querySelector('#current-'+ activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
 
-document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    // annonynouse function, do something here\\
+
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6 ) + 1;
+
+    // 2. display result
+    var diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+
+
+    // 3. display the round score if the rolled number was NOt a 1 
+    if(dice !== 1){
+        //add score
+        roundScore += dice;
+       document.querySelector('#current-'+ activePlayer).textContent = roundScore;
+
+    }else{
+        //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0 ; // same as writeing another if stament it ses "if active player === 0{change active player = 1} else {active player = 0}"
+        roundScore = 0;
+
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+         document.querySelector('.player-0-panel').classList.toggle('active');
+         document.querySelector('.player-1-panel').classList.toggle('active');
+
+
+        // document.querySelector('.player-0-panel').classList.remove('active');
+        // document.querySelector('.player-1-panel').classList.add('active');
+
+
+}
+});
+
+
+// document.querySelector('#current-'+ activePlayer).textContent = dice; /// textContent changes onlhy text
+// document.querySelector('#current-' + activePlayer).innerHTML = "<em>" + dice + "</em>";
+
+
+// var x = document.querySelector('#score-0').textContent;
